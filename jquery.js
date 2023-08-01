48b4306be8448b779285795c9bb9b1fa8d3a7f64
KireMK <137001246+KireMK@users.noreply.github.com>
2023-08-01 15:05:06 +0200

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,78 @@
+//home page//
+
+$('.banner1').hover(function(){
+    $('.nas').fadeIn(1500);
+});
+
+$('.banner1').hover(function(){
+    $('.btnsn').fadeIn(1500);
+});
+
+//About us page//
+
+$('.abusbanner').hover(function(){
+    $('.nas').fadeIn(1500);
+});
+
+//Membership page//
+
+$('.membanner').hover(function(){
+    $('.nas').fadeIn(1500);
+});
+
+$('.membanner').hover(function(){
+    $('.btnsn').fadeIn(1500);
+});
+
+$('.membtn').click(function(){
+  $('.card1').slideToggle();
+})
+
+$('.membtn').click(function(){
+    $('.card2').slideToggle();
+})
+
+$('.membtn').click(function(){
+    $('.card3').slideToggle();
+})
+
+//Programs Page//
+
+$('.progbanner').hover(function(){
+    $('.nas').fadeIn(1500);
+});
+
+$('.progbanner').hover(function(){
+    $('.btnsn').fadeIn(1500);
+});
+
+$('.hoverpr').hover(function(){
+    $('.fitnesspr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.crossfitpr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.yogapr a').fadeIn(2000);
+})
+
+//Fitness page
+
+$('.col-sm-4').hover(function(){
+    $('.fitnesspr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.crossfitpr a').fadeIn(2000);
+})
+
+$('.hoverpr').hover(function(){
+    $('.yogapr a').fadeIn(2000);
+})
+
+//Contact page//
+ $('.cusbanner').hover(function(){
+    $('.nas').fadeIn(500);
+ });
